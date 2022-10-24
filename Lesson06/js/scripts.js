bd72aa506c2f954b1b26dae7859e9055b0c03921
xjomwnd <mwa21001@byui.edu>
2022-10-24 22:54:15 +0300

    commit

---
+++ Lesson06/js/scripts.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu;
+
+("#date2").textContent = fulldate;
