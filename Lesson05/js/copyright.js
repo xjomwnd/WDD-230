8bc5aa85a716647e071f82f99799c53f72b7c468
xjomwnd <mwa21001@byui.edu>
2022-10-25 03:09:29 +0300

    commit

---
+++ Lesson05/js/copyright.js
@@ -0,0 +1,31 @@
+/* ==================copyright year.js================= */
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = `${year}`;
+document.querySelector("#year").textContent = fulldate;
+/* ================== end copyright year.js================= */
