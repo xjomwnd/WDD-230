c77ccec17836486170c9a5264fab9826bf3a1204
xjomwnd <mwa21001@byui.edu>
2022-10-25 12:21:49 +0300

    commit

--- Lesson05/js/scripts.js
+++ Lesson05/js/scripts.js
@@ -1,3 +1,4 @@
+/* ================================toggleMenu.js========================= */
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
@@ -7,3 +8,20 @@ const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
 
 ("#date2").textContent = fulldate;
+
+
+/*==========================banner.js===========================*/
+let day = date.getDay();
+day = 1;
+
+if (day == 1 || day == 2) {
+    let banner = document.querySelector('.banner');
+    banner.style.display = 'block';
+
+    document.querySelector(".banner-button").addEventListener("click", function () {
+        banner.style.display = 'none';
+      });
+} else {
+    let banner = document.querySelector('.banner');
+    banner.style.display = 'none';
+}
