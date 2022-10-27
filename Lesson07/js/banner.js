b9df1532c2c4d90536ab6a9278d4922d8f6027b7
xjomwnd <mwa21001@byui.edu>
2022-10-27 19:14:59 +0300

    COMMIT

--- Lesson07/js/banner.js
+++ Lesson07/js/banner.js
@@ -1,24 +1,17 @@
-let day = date.getDay();
-day = 1;
+document.querySelector(".date").textContent = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(new Date());
 
-if (day == 1 || day == 2) {
-    let banner = document.querySelector('.banner');
-    banner.style.display = 'block';
-
-    document.querySelector(".banner-button").addEventListener("click", function () {
-        banner.style.display = 'none';
-      });
-} else {
-    let banner = document.querySelector('.banner');
-    banner.style.display = 'none';
-}
+document.querySelector('#year').textContent = new Date().getFullYear();
+document.querySelector('#lastupdated').textContent = `Last Modification: ${document.lastModified}`;
 
 
-/*======================banner.js===============================*/
-const weekday = new Date().setDate();
-if (weekday == 1) {
-    document.querySelector('.banner').style.display = "block";
-}
-if (weekday == 2) {
-    document.querySelector('.banner').style.display = "block";
-}
+// ///////////// //
+// Banner JoinUs //
+// ///////////// //
+
+const day = new Date();
+const today = day.getDay();
+
+if (today < 1 || today > 2) {
+    const banner = document.querySelector('.joinUs');
+    banner.remove();
+} 
