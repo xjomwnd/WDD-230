5e7aae4cbeae1a896603c5b13eb2343d3974abe9
xjomwnd <mwa21001@byui.edu>
2022-10-27 19:21:13 +0300

    COMMIT

--- Lesson05/js/banner.js
+++ Lesson05/js/banner.js
@@ -14,23 +14,4 @@ if (day == 1 || day == 2) {
 }
 
 
-/*======================banner.js===============================*/
-
-document.querySelector(".date").textContent = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(new Date());
-
-document.querySelector('#year').textContent = new Date().getFullYear();
-document.querySelector('#lastupdated').textContent = `Last Modification: ${document.lastModified}`;
-
-
-// ///////////// //
-// Banner JoinUs //
-// ///////////// //
-
-const day = new Date();
-const today = day.getDay();
-
-if (today < 1 || today > 2) {
-    const banner = document.querySelector('.joinUs');
-    banner.remove();
-} 
 
