52b7dd2cda3c842288a2761f166d4878c804649a
xjomwnd <mwa21001@byui.edu>
2022-10-27 06:26:08 +0300

    commit

--- Lesson05/banner.js
+++ Lesson05/banner.js
@@ -15,12 +15,6 @@ if (day == 1 || day == 2) {
 
 
 /*======================banner.js===============================*/
-const weekday = new Date().setDate();
-if (weekday == 1) {
-    document.querySelector('.banner').style.display = "block";
-}
-if (weekday == 2) {
-    document.querySelector('.banner').style.display = "block";
-}
+
 
 
