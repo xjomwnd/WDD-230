c30e302dd2c8d57018ab2b4708bb92f801ca0ae2
xjomwnd <mwa21001@byui.edu>
2022-10-27 18:58:47 +0300

    COMMIT

--- Lesson05/js/scripts.js
+++ Lesson05/js/scripts.js
@@ -10,18 +10,3 @@ x.onclick = toggleMenu;
 ("#date2").textContent = fulldate;
 
 
-/*==========================banner.js===========================*/
-let day = date.getDay();
-day = 1;
-
-if (day == 1 || day == 2) {
-    let banner = document.querySelector('.banner');
-    banner.style.display = 'block';
-
-    document.querySelector(".banner-button").addEventListener("click", function () {
-        banner.style.display = 'none';
-      });
-} else {
-    let banner = document.querySelector('.banner');
-    banner.style.display = 'none';
-}
