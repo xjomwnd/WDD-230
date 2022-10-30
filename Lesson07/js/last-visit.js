61d70ccc00c82ddce33028abba49d6f8ef55586d
xjomwnd <mwa21001@byui.edu>
2022-10-30 19:50:04 +0300

    commit

---
+++ Lesson07/js/last-visit.js
@@ -0,0 +1,18 @@
+//* LAST VISIT js*//
+// ///////////// //
+
+let dayBefore = localStorage.getItem('date');
+
+localStorage.setItem('date', day);
+
+dayBefore = Date.parse(dayBefore);
+
+const number_mSdayS = (day - dayBefore)/86400000;
+
+
+if (!number_mSdayS) {
+    document.querySelector("#lastVisit").textContent = 0
+}
+else {
+    document.querySelector("#lastVisit").textContent = number_mSdayS.toFixed(0);
+}
