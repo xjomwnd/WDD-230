3ce835356d11c410e6d17e0db3d64fefa48155dd
xjomwnd <mwa21001@byui.edu>
2022-10-31 12:32:27 +0300

    commit

--- Lesson05/js/banner.js
+++ Lesson05/js/banner.js
@@ -7,22 +7,3 @@ if (day == 1 || day == 2) {
 } else {
     banner.remove();
 }
-
-
-
-
-/* ============================================================ */
-let day = date.getDay();
-day = 1;
-
-if (day == 1 || day == 2) {
-    let banner = document.querySelector('.banner');
-    banner.style.display = 'block';
-
-    document.querySelector(".banner-button").addEventListener("click", function () {
-        banner.style.display = 'none';
-      });
-} else {
-    let banner = document.querySelector('.banner');
-    banner.style.display = 'none';
-}
