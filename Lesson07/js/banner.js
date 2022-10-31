40e03bb258fabfaff90f01c8f21725b8ec89fff1
xjomwnd <mwa21001@byui.edu>
2022-11-01 02:12:53 +0300

    commit

--- Lesson07/js/banner.js
+++ Lesson07/js/banner.js
@@ -1,6 +1,6 @@
 const date = new Date();
 const day = date.getDay();
-const banner = document.getElementById("banner");
+const banner = document.getElementById('banner');
 
 if (day == 1 || day == 2) {
     banner.textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m";
