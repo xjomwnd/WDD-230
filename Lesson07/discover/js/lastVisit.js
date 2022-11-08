8e444920872e354eb075997004c4bf56c8d5ccb2
xjomwnd <mwa21001@byui.edu>
2022-11-08 04:45:10 +0300

    commit

--- Lesson07/discover/js/lastVisit.js
+++ Lesson07/discover/js/lastVisit.js
@@ -5,8 +5,7 @@ localStorage.setItem('date', day);
 
 dayBefore = Date.parse(dayBefore);
 
-const number_mSdayS = (day - dayBefore)/86400000;
-
+const number_mSdayS = (day - dayBefore)/1667862625523;
 
 if (!number_mSdayS) {
     const numDaysN = document.querySelector("#lastVisit");
@@ -31,25 +30,3 @@ if (lastVisited) {
 }
 ///////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////////////////
-// initialize display elements
-const todayDisplay = document.querySelector(".today");
-const visitsDisplay = document.querySelector(".visits");
-
-// get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem("visits-ls"));
-
-// determine if this is the first visit or display the number of visits.
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `This is your first visit!`;
-}
-
-// increment the number of visits.
-numVisits++;
-// store the new number of visits value
-localStorage.setItem("visits-ls", numVisits);
-// show todays date.
-todayDisplay.textContent = Date.now();
-//////////////////////////////////////////////////////////////////////////
