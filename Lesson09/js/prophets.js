4b91ccc52f0ed5a4884dee1f922ede6e79d72a3e
xjomwnd <mwa21001@byui.edu>
2022-11-08 11:05:17 +0300

    commit

--- Lesson09/js/prophets.js
+++ Lesson09/js/prophets.js
@@ -1,4 +1,5 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const cards = document.querySelector('.cards');
 
 async function getData(requestURL) {
     const response = await fetch(requestURL);
