f7bc77f540e21ddc64bc296d4a552b30cbb20cf2
xjomwnd <mwa21001@byui.edu>
2022-11-08 12:00:11 +0300

    commit

--- Lesson09/js/prophets.js
+++ Lesson09/js/prophets.js
@@ -1,3 +1,4 @@
+/*///////////////////////////////////////////////////////////////////////////////////////////////////////
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 const cards = document.querySelector('.cards');
 
@@ -64,4 +65,17 @@ function displayTable(prophet) {
 
     document.querySelector('table').appendChild(tableRow);
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////*/
 
+/////////////////////////////////////////////////////////w09//////////////////////////////////////////////
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const cards = document.querySelector('.cards');
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+  });
+/////////////////////////////////////////////////////////w09/////////////////////////////////////////////
