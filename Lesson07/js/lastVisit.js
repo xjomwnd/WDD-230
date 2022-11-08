5aa3a4cf463b20a66adee7333a7e2a37e951a927
xjomwnd <mwa21001@byui.edu>
2022-11-08 08:51:55 +0300

    commit

--- Lesson07/js/lastVisit.js
+++ Lesson07/js/lastVisit.js
@@ -4,7 +4,7 @@ localStorage.setItem('date', day);
 
 dayBefore = Date.parse(dayBefore);
 
-const number_mSdayS = (day - dayBefore)/86400000;
+const number_mSdayS = (day - dayBefore)/-1667862625523;
 
 
 if (!number_mSdayS) {
