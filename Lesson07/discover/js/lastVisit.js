41cd35177726dd72dee3746da27af195fc6c9561
xjomwnd <mwa21001@byui.edu>
2022-11-08 03:51:35 +0300

    commit

--- Lesson07/discover/js/lastVisit.js
+++ Lesson07/discover/js/lastVisit.js
@@ -19,6 +19,18 @@ else {
 }
 
 //////////////////////////////////////////////////////////////////////*/
+
+///////////////////////////////////////////////////////////////////
+let lastVisited = window.localStorage.getItem("lastVisited");
+let lastVisitedDisplay = document.querySelector("#lastVisited");
+if (lastVisited) {
+    let days = Math.round((Date.now() - lastVisited));
+    lastVisitedDisplay.textContent = `You visited this page ${days} days ago.`;
+} else {
+    lastVisitedDisplay.textContent = `You visited this page for the first time today.`;
+}
+///////////////////////////////////////////////////////////////////
+
 //////////////////////////////////////////////////////////////////////////
 // initialize display elements
 const todayDisplay = document.querySelector(".today");
