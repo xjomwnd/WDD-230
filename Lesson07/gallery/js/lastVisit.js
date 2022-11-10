c1df5bcb7825b846d9b1e4d973f66744555dc8ee
xjomwnd <mwa21001@byui.edu>
2022-11-10 07:18:53 +0300

    commit

--- Lesson07/gallery/js/lastVisit.js
+++ Lesson07/gallery/js/lastVisit.js
@@ -1,39 +1,26 @@
-/*///////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
 let dayBefore = localStorage.getItem('date');
 
 localStorage.setItem('date', day);
 
 dayBefore = Date.parse(dayBefore);
 
-const number_mSdayS = (day - dayBefore)/-946673996398;
+const number_mSdayS = (day - dayBefore)/86400000;
 
 
 if (!number_mSdayS) {
-    document.querySelector("#lastVisit").textContent = 0
+    const numDaysN = document.querySelector("#lastVisit");
+    if (numDaysN) { numDaysN.textContent = 0; }
 }
+
 else {
-    document.querySelector("#lastVisit").textContent = number_mSdayS.toFixed(0);
+    const numDaysY = document.querySelector("#lastVisit");
+    if (numDaysY) numDaysY.textContent = number_mSdayS.toFixed(0);
 }
 
-////////////////////////////////////////////////////////////////////////////*/
- const visitsDisplay = document.querySelector("#lastVisited");
+///////////////////////////////////////////////////////////////////////
 
-// // get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem("visits-ls"));
-
-// // determine if this is the first visit or display the number of visits.
-if (numVisits !== 0) {
- 	visitsDisplay.textContent = numVisits;
- } else {
- 	visitsDisplay.textContent = `This is your first visit!`;
- }
-
-// // increment the number of visits.
- numVisits++;
-// // store the new number of visits value
- localStorage.setItem("visits-ls", numVisits);
- //////////////////////////////////////////////////////////////////////////////////
- // last visited in localStorage and display it in days
+///////////////////////////////////////////////////////////////////
 let lastVisited = window.localStorage.getItem("lastVisited");
 let lastVisitedDisplay = document.querySelector("#lastVisited");
 if (lastVisited) {
@@ -42,3 +29,4 @@ if (lastVisited) {
 } else {
     lastVisitedDisplay.textContent = `You visited this page for the first time today.`;
 }
+///////////////////////////////////////////////////////////////////
