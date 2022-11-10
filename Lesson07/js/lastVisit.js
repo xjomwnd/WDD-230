7d2f9349f9c104ffd0bee1f204d85c850872c61a
xjomwnd <mwa21001@byui.edu>
2022-11-10 06:32:15 +0300

    commit

--- Lesson07/js/lastVisit.js
+++ Lesson07/js/lastVisit.js
@@ -1,10 +1,11 @@
+/*
 let dayBefore = localStorage.getItem('date');
 
 localStorage.setItem('date', day);
 
 dayBefore = Date.parse(dayBefore);
 
-const number_mSdayS = (day - dayBefore)/-dayBefore;
+const number_mSdayS = (day - dayBefore)/86400000;
 
 
 if (!number_mSdayS) {
@@ -12,4 +13,25 @@ if (!number_mSdayS) {
 }
 else {
     document.querySelector("#lastVisit").textContent = number_mSdayS.toFixed(0);
+}
+*/
+
+/* ////////////////// lastVisit-js ////////////////////// */
+let dayBefore = localStorage.getItem('date');
+
+localStorage.setItem('date', day);
+
+dayBefore = Date.parse(dayBefore);
+
+const number_mSdayS = (day - dayBefore)/86400000;
+
+
+if (!number_mSdayS) {
+    const numDaysN = document.querySelector("#lastVisit");
+    if (numDaysN) { numDaysN.textContent = 0; }
+}
+
+else {
+    const numDaysY = document.querySelector("#lastVisit");
+    if (numDaysY) numDaysY.textContent = number_mSdayS.toFixed(0);
 }
