b56f8d21c5e9bc3dd5318260ad33b884347da243
xjomwnd <mwa21001@byui.edu>
2022-11-12 13:14:36 +0300

    commit

--- Lesson09/js/directory.js
+++ Lesson09/js/directory.js
@@ -1,3 +1,40 @@
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 const requestURL='https://github.com/xjomwnd/WDD-230-1/blob/main/Lesson09/data.json';
 document.addEventListener("DOMContentLoaded", () => {
     fetch(requestURL)
