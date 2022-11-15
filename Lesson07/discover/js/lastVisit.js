421e4b1999d00d399aae2705409499e5acb2e84f
xjomwnd <mwa21001@byui.edu>
2022-11-16 02:37:28 +0300

    commit

--- Lesson07/discover/js/lastVisit.js
+++ Lesson07/discover/js/lastVisit.js
@@ -1,32 +1,18 @@
-/////////////////////////////////////////////////////////////////
-let dayBefore = localStorage.getItem('date');
+const visitsDisplay = document.querySelector(".visits");
 
-localStorage.setItem('date', day);
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
 
-dayBefore = Date.parse(dayBefore);
-
-const number_mSdayS = (day - dayBefore)/86400000;
-
-
-if (!number_mSdayS) {
-    const numDaysN = document.querySelector("#lastVisit");
-    if (numDaysN) { numDaysN.textContent = 0; }
-}
-
-else {
-    const numDaysY = document.querySelector("#lastVisit");
-    if (numDaysY) numDaysY.textContent = number_mSdayS.toFixed(0);
-}
-///////////////////////////////////////////////////////////////////////
-
-///////////////////////////////////////////////////////////////////
-let lastVisited = window.localStorage.getItem("lastVisited");
-let lastVisitedDisplay = document.querySelector("#lastVisited");
-if (lastVisited) {
-    let days = Math.round((Date.now() - lastVisited)/-1);
-    lastVisitedDisplay.textContent = `You visited this page ${days} days ago.`;
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
 } else {
-    lastVisitedDisplay.textContent = `You visited this page for the first time today.`;
+	visitsDisplay.textContent = `This is your first visit!`;
 }
-///////////////////////////////////////////////////////////////////
 
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+// show todays date.
+todayDisplay.textContent = Date.now();
