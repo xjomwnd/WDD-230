e4bd260e2d7e6705e486502571f847e7ca3eaf6c
xjomwnd <mwa21001@byui.edu>
2022-11-16 22:12:38 +0300

    commit

--- Lesson07/discover/js/year.js
+++ Lesson07/discover/js/year.js
@@ -1,11 +1,3 @@
-/* =================== last-modfied.js large ====plus the==== copyright.js========================== */
-/* ==================copyright year================= */
-let lastModified = new Date(document.lastModified);
-let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
-let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
-let dateTime = `Last Updated: ${fullDate} ${time}`;
-document.getElementById("lastModified").innerHTML = dateTime;
-/*===================================== copyright.js =====================================*/
 const daynames = [
 	"Sunday",
 	"Monday",
