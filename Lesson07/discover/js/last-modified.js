68996c423751409f407aedc16f5baca8e4593e09
xjomwnd <mwa21001@byui.edu>
2022-11-16 03:22:36 +0300

    commit

--- Lesson07/discover/js/last-modified.js
+++ Lesson07/discover/js/last-modified.js
@@ -4,4 +4,38 @@ let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-d
 let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
 let dateTime = `Last Updated: ${fullDate} ${time}`;
 document.getElementById("lastModified").innerHTML = dateTime;
-/* ==================end last-modified.js================= */
+//* ==================end last-modified.js================= */
+
+
+
+/* ==================copyright year================= */
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = `${year}`;
+document.querySelector("#year").textContent = fulldate;
+/* ================== END copyright year.js================= */
