202db857b4eb93a05ba9e173b7a48de62ae5e583
xjomwnd <mwa21001@byui.edu>
2022-11-17 12:27:42 +0300

    commit

--- Lesson10/js/weatherapi.js
+++ Lesson10/js/weatherapi.js
@@ -1,26 +1,21 @@
-/////////////////////////////////////////////////////////////////////////////////////
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5347315&appid=70576274fb494d1b32471985c55c97f3'
-
+//////////////////////////////////////////////////////////////////////////////////*/
+const apiURL ='https://api.openweathermap.org/data/2.5/weather?id=5861897&appid=70576274fb494d1b32471985c55c97f3'
 const getWeather = async () => {
     const response = await fetch(apiURL);
     jsObject = await response.json();
     console.log(jsObject);
-
     let temp = kelvinToFahrenheit(jsObject.main.temp);
     document.querySelector('#current-temp').textContent = temp;
-
     const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const desc = jsObject.weather[0].description;
     document.querySelector('#icon-src').textContent = iconsrc;
     document.querySelector('#weathericon').setAttribute('src', iconsrc);
     document.querySelector('#weathericon').setAttribute('alt', desc);
     document.querySelector('figcaption').textContent = desc;
-
 };
-
 const kelvinToFahrenheit = (kelvin) => {
     const f = (kelvin - 273.15) * 1.8 + 32;
     return f.toFixed(2);
 }
-
 getWeather();
+
