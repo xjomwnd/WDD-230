dff38eac7673d1e3bd44e6aa3bfe733c40cd4180
xjomwnd <mwa21001@byui.edu>
2022-11-18 12:20:44 +0300

    commit

---
+++ Lesson07/discover/js/#lastVisit.js
@@ -0,0 +1,31 @@
+let dayBefore = localStorage.getItem('date');
+
+localStorage.setItem('date', day);
+
+dayBefore = Date.parse(dayBefore);
+
+const number_mSdayS = (day - dayBefore)/86400000;
+
+
+if (!number_mSdayS) {
+    const numDaysN = document.querySelector("#lastVisit");
+    if (numDaysN) { numDaysN.textContent = 0; }
+}
+
+else {
+    const numDaysY = document.querySelector("#lastVisit");
+    if (numDaysY) numDaysY.textContent = number_mSdayS.toFixed(0);
+}
+
+//////////////////////////////////////////////////////////////////////*/
+
+///////////////////////////////////////////////////////////////////
+let lastVisited = window.localStorage.getItem("lastVisited");
+let lastVisitedDisplay = document.querySelector("#lastVisited");
+if (lastVisited) {
+    let days = Math.round((Date.now() - lastVisited));
+    lastVisitedDisplay.textContent = `You visited this page ${days} days ago.`;
+} else {
+    lastVisitedDisplay.textContent = `You visited this page for the first time today.`;
+}
+///////////////////////////////////////////////////////////////////
