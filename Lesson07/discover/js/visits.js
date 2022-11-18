ebcd1876c3a6590795eeb9ca6b3664c4b90232bb
xjomwnd <mwa21001@byui.edu>
2022-11-18 12:06:07 +0300

    commit

--- Lesson07/discover/js/visits.js
+++ Lesson07/discover/js/visits.js
@@ -1,4 +1,4 @@
-/////////////////////////////////////////////////////////////////////
+/*///////////////////////////////////////////////////////////////////*/
 // initialize display elements
 const todayDisplay = document.querySelector(".today");
 const visitsDisplay = document.querySelector(".visits");
@@ -21,20 +21,3 @@ localStorage.setItem("visits-ls", numVisits);
 todayDisplay.textContent = Date.now();
 
 ///////////////////////////////////////////////////////////////////////
-/////////////////////////////////////NUM OF VISITTS JS/////////////////////////////////
-
-const visitsDisplay = document.querySelector(".visits");
-// get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem("visits-ls"));
-// determine if this is the first visit or display the number of visits.
-if (numVisits !== 0) {
-    visitsDisplay.textContent = numVisits;
-} else {
-    visitsDisplay.textContent = `This is your first visit!`;
-}
-// increment the number of visits.
-numVisits++;
-// store the new number of visits value
-localStorage.setItem("visits-ls", numVisits);
-// show todays date.
-todayDisplay.textContent = Date.now();
