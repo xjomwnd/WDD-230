9cae161b65524b76f53a9cc83cd0760accdbac80
xjomwnd <mwa21001@byui.edu>
2022-11-18 11:12:38 +0300

    COMMIT

--- Lesson07/discover/js/lastVisit.js
+++ Lesson07/discover/js/lastVisit.js
@@ -16,3 +16,26 @@ numVisits++;
 localStorage.setItem("visits-ls", numVisits);
 // show todays date.
 todayDisplay.textContent = Date.now();
+
+/////////////////////////////////// NUMBER OF VISITS JS///////////////////////
+/////////// LAST VISIT JS//////////////
+
+letdayBefore=localStorage.getItem('date');
+
+
+localStorage.setItem('date',day);
+
+dayBefore=Date.parse(dayBefore);
+
+constnumber_mSdayS=(day-dayBefore)/86400000;
+
+
+if(!number_mSdayS){
+    constnumDaysN=document.querySelector("#lastVisit");
+    if(numDaysN){numDaysN.textContent=0;}
+}
+
+else{
+    constnumDaysY=document.querySelector("#lastVisit");
+    if(numDaysY)numDaysY.textContent=number_mSdayS.toFixed(0);
+}
