d2bde697ead29f4983891a231804699c610e5fc8
xjomwnd <mwa21001@byui.edu>
2022-11-21 00:47:39 +0300

    commit

--- Lesson06/js/last-modified.js
+++ Lesson06/js/last-modified.js
@@ -1,16 +1,14 @@
-document.querySelector(
-	"#lastModified"
-).textContent = `Last Modification: ${document.lastModified}`;
-
-
-/* =================== last-modfied.js large ====plus the==== copyright.js========================== */
-/* ==================copyright year================= */
+/* ==================last-modified.js================= */
 let lastModified = new Date(document.lastModified);
 let fullDate = lastModified.toLocaleString('en-US', {month: "2-digit", day: "2-digit", year: "numeric"});
 let time = lastModified.toLocaleString('en-GB', {hour: "2-digit", minute: "2-digit", second: "2-digit"});
 let dateTime = `Last Updated: ${fullDate} ${time}`;
 document.getElementById("lastModified").innerHTML = dateTime;
-/*===================================== copyright.js =====================================*/
+//* ==================end last-modified.js================= */
+
+
+
+/* ==================copyright year================= */
 const daynames = [
 	"Sunday",
 	"Monday",
@@ -40,4 +38,4 @@ const monthName = months[d.getMonth()];
 const year = d.getFullYear();
 const fulldate = `${year}`;
 document.querySelector("#year").textContent = fulldate;
-/* ================== end copyright year.js================= */
+/* ================== END copyright year.js================= */
