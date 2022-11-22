aee880f3ba0b91b86b51e99a676abcfe40ef1047
xjomwnd <mwa21001@byui.edu>
2022-11-22 20:46:08 +0300

    COMMIT

--- Lesson10/js/weatherapi.js
+++ Lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-///////////////////////////////////////////////////////////////////////////////
+/*//////////////////////////////////////////////////////////////////////////////
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5809844&appid=70576274fb494d1b32471985c55c97f3'
 
 const getWeather = async () => {
@@ -24,13 +24,12 @@ const kelvinToFahrenheit = (kelvin) => {
 }
 
 getWeather();
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*/////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////*/
+//////////////////////////////////////////////////////////////////////////////////////////////
 // select output elements
 const temperature = document.querySelector('#temperature');
-const description = document.querySelector('#currently');
 const weathericon = document.querySelector('#weathericon');
-const caption = document.querySelector('#figcaption');
+const caption = document.querySelector('figcaption');
 
 // Get the weather data from the API - openweather.org
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5861897&appid=70576274fb494d1b32471985c55c97f3'
@@ -40,20 +39,22 @@ apiFetch();
 //capitalize
 
 async function apiFetch(){
+try{
+    const response = await fetch(apiURL);
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        displayResults(data);
+    } else {
+        console.log('Response not ok ${await response.text()}');
+    }
+}catch(error){
+    console.log('Error:${error.message}');
+}
 }
 apiFetch()
 //capitalize
 
 function capitalize(string) {
-    return '${string.charAt(0).toupperCase()} ${string.slice(1)}';
+    return '${string.charAt(0).toUpperCase()}${string.slice(1)}';
 }
-// display the results
-function displayResults(data) {
-    temperature.textContent = data.main.temp.toFixed(0);
-    let desc = capitalize(data.weather[0].description);
-    description.textContent = desc;
-    caption.textContent = 'Icon of current weather condition at Fairbanks which is ${desc}';
-    weathericon.src = 'https://openweathermap.org/img/w/${data.weather[0].icon}.png';
-    weathericon.alt = 'Icon of current weather condition at Fairbanks which is ${desc}';
-}
-/////////////////////////////////////////////////////////////////////////////////////////*/
