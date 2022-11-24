6423f8246938cc876c5baaef87418fcc3ded72f7
xjomwnd <mwa21001@byui.edu>
2022-11-24 14:05:16 +0300

    commit

--- Lesson06/js/hamburger.js
+++ Lesson06/js/hamburger.js
@@ -1,8 +1,6 @@
 ///////////////////////////////////////////////////////////
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
-
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
