646c1bc2b1b3425ad74f0953addc129fad6ff54c
xjomwnd <mwa21001@byui.edu>
2022-11-24 23:47:53 +0300

    commit

--- Lesson10/js/weatherapi.js
+++ Lesson10/js/weatherapi.js
@@ -1,3 +1,4 @@
+/*//////////////////////////////////////////////////////////////////
 const weather = document.querySelector('.weatherInfo');
 const temperature = document.querySelector('#temperature');
 const weathericon = document.querySelector('#weathericon');
@@ -91,3 +92,50 @@ const caption = document.querySelector('figcaption');
   }
   if (weather) { apiFetch(); }
 
+//////////////////////////////////////////////////////////*/
+
+// select HTML elements in the document
+const weather = document.querySelector('.weatherInfo');
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?id=5809844&appid=70576274fb494d1b32471985c55c97f3';
+
+// Get the weather data from the API - openweather.org 
+function capital_letter(str) 
+{
+    str = str.split(" ");
+    for (var i = 0; i < str.length; i++) {
+        str[i] = `${str[i][0].toUpperCase()}${str[i].substr(1)}`;
+    }
+    return str.join(" ");
+}
+
+function  displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+}
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      // displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
