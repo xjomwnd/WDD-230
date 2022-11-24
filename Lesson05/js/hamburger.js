940108035755664400bc0eb43d3e512936c72df5
xjomwnd <mwa21001@byui.edu>
2022-11-24 13:02:27 +0300

    commit

--- Lesson05/js/hamburger.js
+++ Lesson05/js/hamburger.js
@@ -6,7 +6,7 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 
-/*** Programming Notes **************************************
+/* Programming Notes **************************************
   Arrow Functions - es6 syntactically compact alternative to a regular function expression
   see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
   or https://www.w3schools.com/js/js_arrow_function.asp
