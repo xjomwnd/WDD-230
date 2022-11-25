0ef75699b046376a11b33c46973b297253229342
xjomwnd <mwa21001@byui.edu>
2022-11-26 00:57:55 +0300

    commit

--- Lesson06/js/index.js
+++ Lesson06/js/index.js
@@ -1,4 +1,44 @@
-// Today's Date
-const dateField = document.querySelector(".date");
-const fullDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
-dateField.innerHTML = fullDate
+const date1 = document.querySelector("#date1");
+// Try to complete the method with options
+try {
+	const options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+		year: "numeric"
+	};
+	date1.textContent = new Date().toLocaleDateString("en-UK", options);
+} catch (e) {
+	alert("Error with code or your browser does not support Locale");
+}
+
+// Long hand method ... building day and month names from built-in date methods.
+
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
