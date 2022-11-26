6a5bd6d3aad6e940d1990f5f7e46a655cacc3d6c
xjomwnd <mwa21001@byui.edu>
2022-11-26 05:40:11 +0300

    commit

--- Lesson06/js/hamburger.js
+++ Lesson06/js/hamburger.js
@@ -1,4 +1,3 @@
 const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
-
+const mainnav = document.querySelector('.navigation')s
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
