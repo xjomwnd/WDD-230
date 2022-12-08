e1f354b0aac01081ad56a23a94bd53391d124e2e
xjomwnd <mwa21001@byui.edu>
2022-12-08 17:49:17 +0300

    commit

---
+++ Lesson13/js/fetch.js
@@ -0,0 +1,25 @@
+document.querySelector("#retrieve-resources").onclick = () => {
+    let displayResources = document.querySelector("#display-resources");
+    displayResources.textContent = "Loading data from JSON source...";
+    fetch("https://api.myjson.com/bins/owmcc")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(result) {
+        let output =
+          "<table><thead><tr><th>Name</th><th>Provider</th><th>URL</th></thead><tbody>";
+        for (let i in result) {
+          output +=
+            "<tr><td>" +
+            result[i].name +
+            "</td><td>" +
+            result[i].provider +
+            "</td><td>" +
+            result[i].url +
+            "</td></tr>";
+        }
+        output += "</tbody></table>";
+        displayResources.innerHTML = output;
+      });
+  };
+  
