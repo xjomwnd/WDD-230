8e35e0c81a7ac933ddd8896a275c0b48ad5152d6
xjomwnd <mwa21001@byui.edu>
2022-12-10 22:38:40 +0300

    commit

--- Lesson13/js/function.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("phone").value; 
-    document.getElementById("phone-output").innerHTML = x;
-      var x = document.getElementById("email").value; 
-    document.getElementById("email-output").innerHTML = x; 
-     var x = document.getElementById("order").value; 
-    document.getElementById("order-output").innerHTML = x; 
-    
-  }
